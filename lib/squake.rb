a2049bdc296fa1fccedf73c9f7b6537cb8a7b2a7
Lud <lud@reinmiedl.com>
2023-06-15 17:19:08 +0200

    configure compat mode for Oj

--- lib/squake.rb
+++ lib/squake.rb
@@ -8,3 +8,8 @@ require 'net/http'
 Dir[File.join(__dir__, './**/*', '*.rb')].each { require(_1) }
 
 module Squake; end
+
+Oj.default_options = {
+  mode: :compat, # required to dump hashes with symbol-keys
+  symbol_keys: true,
+}
