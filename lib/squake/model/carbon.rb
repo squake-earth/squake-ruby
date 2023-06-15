de18abd212390b7174ff61eb5af6290c0dec9d4f
Lud <lud@reinmiedl.com>
2023-06-15 16:42:40 +0200

    require cu

--- lib/squake/model/carbon.rb
+++ lib/squake/model/carbon.rb
@@ -1,33 +1,35 @@
 # typed: strict
 # frozen_string_literal: true
 
+require_relative 'carbon_unit'
+
 module Squake
   module Model
     class Carbon < T::Struct
       extend T::Sig
 
       const :quantity, BigDecimal
-      const :unit, Squake::Model::CarbonUnit, default: Squake::Model::CarbonUnit::GRAM
+      const :unit, CarbonUnit, default: CarbonUnit::GRAM
       const :items, T.nilable(T::Array[T::Hash[Symbol, T.untyped]]) # @TODO: resolve to typed items?
 
       sig { params(response_body: T::Hash[Symbol, T.untyped]).returns(Squake::Model::Carbon) }
       def self.from_api_response(response_body)
         Squake::Model::Carbon.new(
           quantity: BigDecimal(String(response_body.fetch(:quantity))),
-          unit: Squake::Model::CarbonUnit.deserialize(response_body.fetch(:unit)),
+          unit: CarbonUnit.deserialize(response_body.fetch(:unit)),
           items: response_body.fetch(:items, nil),
         )
       end
 
       sig do
         params(
-          unit: Squake::Model::CarbonUnit,
+          unit: CarbonUnit,
         ).returns(Carbon)
       end
       def in!(unit)
         # this is a bit hacky, but it optimizes memory usages and we don't need `T.must`
         # everywhere if we were to use a prop instead of a const.
-        @quantity = Squake::Model::CarbonUnit.convert(quantity, self.unit, to: unit)
+        @quantity = CarbonUnit.convert(quantity, self.unit, to: unit)
         @unit = unit
         self
       end
@@ -38,8 +40,8 @@ module Squake
         ).returns(Carbon)
       end
       def +(other)
-        other_qty = Squake::Model::CarbonUnit.convert(other.quantity, other.unit, to: Squake::Model::CarbonUnit::GRAM)
-        self_qty = Squake::Model::CarbonUnit.convert(quantity, unit, to: Squake::Model::CarbonUnit::GRAM)
+        other_qty = CarbonUnit.convert(other.quantity, other.unit, to: CarbonUnit::GRAM)
+        self_qty = CarbonUnit.convert(quantity, unit, to: CarbonUnit::GRAM)
 
         Carbon.new(quantity: other_qty + self_qty)
       end
