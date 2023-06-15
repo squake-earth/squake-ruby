4eefe0c2a481e448eca618cfec3bbc2a242cfef1
Lud <lud@reinmiedl.com>
2023-06-15 16:40:42 +0200

    use full namespace

--- lib/squake/model/carbon.rb
+++ lib/squake/model/carbon.rb
@@ -7,27 +7,27 @@ module Squake
       extend T::Sig
 
       const :quantity, BigDecimal
-      const :unit, CarbonUnit, default: CarbonUnit::GRAM
+      const :unit, Squake::Model::CarbonUnit, default: Squake::Model::CarbonUnit::GRAM
       const :items, T.nilable(T::Array[T::Hash[Symbol, T.untyped]]) # @TODO: resolve to typed items?
 
       sig { params(response_body: T::Hash[Symbol, T.untyped]).returns(Squake::Model::Carbon) }
       def self.from_api_response(response_body)
         Squake::Model::Carbon.new(
           quantity: BigDecimal(String(response_body.fetch(:quantity))),
-          unit: CarbonUnit.deserialize(response_body.fetch(:unit)),
+          unit: Squake::Model::CarbonUnit.deserialize(response_body.fetch(:unit)),
           items: response_body.fetch(:items, nil),
         )
       end
 
       sig do
         params(
-          unit: CarbonUnit,
+          unit: Squake::Model::CarbonUnit,
         ).returns(Carbon)
       end
       def in!(unit)
         # this is a bit hacky, but it optimizes memory usages and we don't need `T.must`
         # everywhere if we were to use a prop instead of a const.
-        @quantity = CarbonUnit.convert(quantity, self.unit, to: unit)
+        @quantity = Squake::Model::CarbonUnit.convert(quantity, self.unit, to: unit)
         @unit = unit
         self
       end
@@ -38,8 +38,8 @@ module Squake
         ).returns(Carbon)
       end
       def +(other)
-        other_qty = CarbonUnit.convert(other.quantity, other.unit, to: CarbonUnit::GRAM)
-        self_qty = CarbonUnit.convert(quantity, unit, to: CarbonUnit::GRAM)
+        other_qty = Squake::Model::CarbonUnit.convert(other.quantity, other.unit, to: Squake::Model::CarbonUnit::GRAM)
+        self_qty = Squake::Model::CarbonUnit.convert(quantity, unit, to: Squake::Model::CarbonUnit::GRAM)
 
         Carbon.new(quantity: other_qty + self_qty)
       end
