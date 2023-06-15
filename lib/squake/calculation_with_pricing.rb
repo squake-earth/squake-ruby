e57d06b4fae2f0a71eda3df5aa3f4e1fd585d027
Lud <lud@reinmiedl.com>
2023-06-15 17:19:23 +0200

    relax type requirements on items

--- lib/squake/calculation_with_pricing.rb
+++ lib/squake/calculation_with_pricing.rb
@@ -11,7 +11,7 @@ module Squake
     sig do
       params(
         client: Squake::Client,
-        items: T::Array[Squake::Model::Items::BaseType],
+        items: T::Array[T.any(Squake::Model::Items::BaseType, T::Hash[T.any(String, Symbol), T.untyped])],
         product: String,
         currency: String,
         carbon_unit: String,
@@ -19,11 +19,16 @@ module Squake
       ).returns(Squake::Model::Pricing)
     end
     def self.quote(client:, items:, product:, currency: 'EUR', carbon_unit: 'gram', expand: [])
+      # @TODO: add typed objects for all possible items. Until then, we allow either a Hash or a T::Struct
+      items.map! do |item|
+        item.is_a?(T::Struct) ? item.serialize : item
+      end
+
       result = client.call(
         path: ENDPOINT,
         method: :post,
         params: {
-          items: items.map(&:serialize),
+          items: items,
           product: product,
           currency: currency,
           carbon_unit: carbon_unit,
